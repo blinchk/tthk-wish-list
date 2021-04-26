bbd7ea752c89249c1cfa35fcfc0543be296dce5b
Nikolas Laus <bchik3@gmail.com>
2021-04-26 21:17:31 +0300

    Add CORS policy.

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"github.com/bredbrains/tthk-wish-list/database"
 	"github.com/bredbrains/tthk-wish-list/endpoints/auth"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*s"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	authAPI := router.Group("/auth")
 	authAPI.POST("/register", auth.Register)
 	database.Connect()
