34bad6f63dc122ff02b02f83346bc6456cbef6d0
Nikolas Laus <bchik3@gmail.com>
2021-05-02 20:05:25 +0300

    Update CreationTime type for SQL compatibility.

--- models/wish.go
+++ models/wish.go
@@ -1,12 +1,10 @@
 package models
 
-import "time"
-
 type Wish struct {
-	ID           uint64    `json:"id"`
-	Name         string    `json:"name"`
-	Description  string    `json:"description"`
-	User         User      `json:"user"`
-	Hidden       bool      `json:"hidden"`
-	CreationTime time.Time `json:"creationTime"`
+	ID           uint64  `json:"id"`
+	Name         string  `json:"name"`
+	Description  string  `json:"description"`
+	User         User    `json:"user"`
+	Hidden       bool    `json:"hidden"`
+	CreationTime []uint8 `json:"creationTime"`
 }
