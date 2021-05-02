ca1cda97389f9281f2a2310736d70179b4ef732b
Nikolas Laus <bchik3@gmail.com>
2021-05-02 20:00:02 +0300

    Update wish model

--- models/wish.go
+++ models/wish.go
@@ -1,9 +1,12 @@
 package models
 
+import "time"
+
 type Wish struct {
-	ID          uint64 `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	User        User   `json:"user"`
-	Hidden      bool   `json:"hidden"`
+	ID           uint64    `json:"id"`
+	Name         string    `json:"name"`
+	Description  string    `json:"description"`
+	User         User      `json:"user"`
+	Hidden       bool      `json:"hidden"`
+	CreationTime time.Time `json:"creationTime"`
 }
