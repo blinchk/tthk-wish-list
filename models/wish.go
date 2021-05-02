792a2a1f3a21ced4ffc4142acaff49ab44d16aef
Nikolas Laus <bchik3@gmail.com>
2021-05-02 20:09:07 +0300

    Concat creationTime to string.

--- models/wish.go
+++ models/wish.go
@@ -1,10 +1,10 @@
 package models
 
 type Wish struct {
-	ID           uint64  `json:"id"`
-	Name         string  `json:"name"`
-	Description  string  `json:"description"`
-	User         User    `json:"user"`
-	Hidden       bool    `json:"hidden"`
-	CreationTime []uint8 `json:"creationTime"`
+	ID           uint64 `json:"id"`
+	Name         string `json:"name"`
+	Description  string `json:"description"`
+	User         User   `json:"user"`
+	Hidden       bool   `json:"hidden"`
+	CreationTime string `json:"creationTime"`
 }
