f83620d43a32e51d3331b5ce6edd701b44f47061
JeF11037 <lev.petryakov@gmail.com>
2021-05-06 16:53:31 +0300

    #140 Change update wish to PATCH method

--- endpoints/wishes/update.go
+++ endpoints/wishes/update.go
@@ -2,25 +2,51 @@ package wishes
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/bredbrains/tthk-wish-list/database"
 	"github.com/bredbrains/tthk-wish-list/models"
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 )
 
 func Update(c *gin.Context) {
 	var wish models.Wish
+	var newWish models.Wish
 	var err error
-	err = c.BindJSON(&wish)
+	var user models.User
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": "Bad ID of wish."})
+		return
+	}
+	err, wish = database.GetWish(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": err.Error()})
+		return
+	}
+	err = c.BindJSON(&newWish)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": err.Error()})
+		return
+	}
+	if newWish.Name == "" && newWish.Description == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": "At least one of the field must be filled."})
+		return
+	}
+	if newWish.Name == "" {
+		newWish.Name = wish.Name
+	}
+	if newWish.Description == "" {
+		newWish.Description = wish.Description
+	}
+	wish.Name, wish.Description = newWish.Name, newWish.Description
+	err, user = database.UserData(c.GetHeader("Token"))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
 		return
 	}
-	v := validator.New()
-	err = v.Struct(wish)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": err.Error()})
+	if wish.User.ID != user.ID {
+		c.JSON(http.StatusUnauthorized, gin.H{"success": false, "error": "You don't have permissions for this."})
 		return
 	}
 	err, wish = database.UpdateWish(wish)
