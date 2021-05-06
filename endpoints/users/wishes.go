f17e4d2bacc9fa42f45670dc4a6ac01049d932a2
JeF11037 <lev.petryakov@gmail.com>
2021-05-06 17:13:59 +0300

    #136 Add new verifications

--- endpoints/users/wishes.go
+++ endpoints/users/wishes.go
@@ -1,22 +1,32 @@
 package users
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/bredbrains/tthk-wish-list/database"
 	"github.com/bredbrains/tthk-wish-list/models"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"strconv"
 )
 
 func Wishes(c *gin.Context) {
 	var wishes []models.Wish
+	var user models.User
 	var err error
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": err.Error()})
 		return
 	}
-	user := models.User{ID: id}
+	err, user = database.UserData(c.GetHeader("Token"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
+		return
+	}
+	if id != user.ID {
+		c.JSON(http.StatusUnauthorized, gin.H{"success": false, "error": "You don't have permissions for this."})
+		return
+	}
 	err, wishes = database.GetWishes(user)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
