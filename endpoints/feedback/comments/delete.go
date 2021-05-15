3a58d54f4eb52c30cfcba0fbecd9e283c7f6167f
JeF11037 <lev.petryakov@gmail.com>
2021-05-15 19:44:36 +0300

    #217 Add comment delete endpoint

--- endpoints/feedback/comments/delete.go
+++ endpoints/feedback/comments/delete.go
@@ -2,11 +2,46 @@ package comments
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/bredbrains/tthk-wish-list/database"
+	"github.com/bredbrains/tthk-wish-list/models"
 	"github.com/gin-gonic/gin"
 )
 
 func Delete(c *gin.Context) {
+	var comment models.Comment
+	var err error
+	var user models.User
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": "Bad ID of comment."})
+		return
+	}
+	token := c.GetHeader("Token")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
+		return
+	}
+	err, user = database.UserData(token)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
+		return
+	}
+	err, comment = database.GetComment(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
+		return
+	}
+	if comment.User.ID != user.ID {
+		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": "No permission."})
+		return
+	}
+	err = database.DeleteComment(comment)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
+		return
+	}
 	message := gin.H{"success": true}
 	c.JSON(http.StatusOK, message)
 	return
