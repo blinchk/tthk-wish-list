c315f8be2750e9bc38883181f100218b240f9ca7
JeF11037 <lev.petryakov@gmail.com>
2021-05-15 19:07:04 +0300

    #216 Add comment add endpoint

--- endpoints/feedback/comments/add.go
+++ endpoints/feedback/comments/add.go
@@ -2,12 +2,38 @@ package comments
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/bredbrains/tthk-wish-list/database"
+	"github.com/bredbrains/tthk-wish-list/models"
 	"github.com/gin-gonic/gin"
 )
 
 func Add(c *gin.Context) {
-	message := gin.H{"success": true}
+	var comment models.Comment
+	var err error
+	err = c.BindJSON(&comment)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": "Request body is invalid."})
+		return
+	}
+	token := c.GetHeader("Token")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
+		return
+	}
+	err, comment.User = database.UserData(token)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
+		return
+	}
+	comment.CreationTime = time.Now().Format("2006-01-02 15:04:05")
+	err, comment = database.AddComment(comment)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
+		return
+	}
+	message := gin.H{"success": true, "comment": comment}
 	c.JSON(http.StatusOK, message)
 	return
 }
