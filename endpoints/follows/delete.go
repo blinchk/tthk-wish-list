24101c23353f34bcbb0fb282914dc2ff83569452
JeF11037 <lev.petryakov@gmail.com>
2021-05-15 19:54:49 +0300

    Add return

--- endpoints/follows/delete.go
+++ endpoints/follows/delete.go
@@ -14,11 +14,14 @@ func Delete(c *gin.Context) {
 	err = c.BindJSON(&follow)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": "Request body is invalid."})
+		return
 	}
 	err = database.DeleteFollow(follow)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
+		return
 	}
 	message := gin.H{"follow": follow}
 	c.JSON(http.StatusOK, message)
+	return
 }
