f831a47a14a35413c97fe092354b25b06016c2b0
JeF11037 <lev.petryakov@gmail.com>
2021-06-07 19:51:03 +0300

    Edit gift method Change method because of gift model changed.

--- endpoints/feedback/gift.go
+++ endpoints/feedback/gift.go
@@ -13,15 +13,34 @@ func ToggleGift(c *gin.Context) {
 	var gift models.Gift
 	var err error
 	var message gin.H
-	id, err := strconv.Atoi(c.Param("id"))
+	user, err := strconv.Atoi(c.Param("user"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": "Bad ID of user."})
 		return
 	}
-	gift.User.ID = id
+	wish, err := strconv.Atoi(c.Param("wish"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": "Bad ID of user."})
+		return
+	}
 	err = c.BindJSON(&gift)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": "Request body is invalid."})
+		return
+	}
+	err, currentUser := database.UserData(c.GetHeader("Token"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": "Invalid token."})
+		return
+	}
+	err, gift.User = database.UserDataById(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
+		return
+	}
+	err, gift.Wish = database.GetWishByIdAndUser(wish, currentUser)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
 		return
 	}
 	if database.GiftExist(gift) {
@@ -47,10 +66,15 @@ func GetGifts(c *gin.Context) {
 	var gifts []models.Gift
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": "Bad ID of wish."})
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": "Bad ID of user."})
 		return
 	}
-	err, gifts = database.GetGiftsByWish(id)
+	err, currentUser := database.UserData(c.GetHeader("Token"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": "Invalid token."})
+		return
+	}
+	err, gifts = database.GetGiftsByUsers(id, currentUser)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
 		return
