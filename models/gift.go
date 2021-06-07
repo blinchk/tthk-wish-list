b7857a31a8f69b5097affe5cede91319ac8435dc
JeF11037 <lev.petryakov@gmail.com>
2021-06-07 19:50:07 +0300

    Edit gift model Add link data and change wish type for flexibility.

--- models/gift.go
+++ models/gift.go
@@ -2,7 +2,8 @@ package models
 
 type Gift struct {
 	ID           int    `json:"id,omitempty"`
-	Wish         int    `json:"wish,omitempty"`
+	Wish         Wish   `json:"wish,omitempty"`
 	User         User   `json:"user,omitempty"`
+	Link         string `json:"link, omtempty"`
 	CreationTime string `json:"creation_time,omitempty"`
 }
